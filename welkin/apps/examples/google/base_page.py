6f6539523916077666b10611f20a8fc9797f4b17
dsisson <philosophe@gmail.com>
2018-02-23 14:19:38 -0800

    google/base_page.py: update content
    + moved verify_self() into HomePage class
    + add search_for()

--- welkin/apps/examples/google/base_page.py
+++ welkin/apps/examples/google/base_page.py
@@ -12,7 +12,26 @@ logger = logging.getLogger(__name__)
 
 class PageObject(object):
 
-    def verify_self(self, driver, path, wait_time=10):
-        wait = WebDriverWait(self.driver, wait_time)
-        pass
+    domain = 'www.google.com'
 
+    def search_for(self, text):
+        """
+            Perform a Google search for string `text`.
+
+            :param text: str, query string
+            :return: None
+        """
+        # grab the search field
+        search_input = self.driver.find_element_by_name('q')
+
+        # pass in the search string
+        search_input.send_keys(text)
+
+        # submit the search
+        search_input.submit()
+
+        # instantiate a search results page
+        from welkin.apps.examples.google.results import SearchResultsPage
+        page = SearchResultsPage(self.driver)
+
+        return page
