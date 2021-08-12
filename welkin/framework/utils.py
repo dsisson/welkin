4e2f783ddeeec6cc4a6033cfc042285ba1ef757c
dsisson <philosophe@gmail.com>
2021-08-12 12:55:46 -0700

    issue 8: update plog() to handle other types
    framework/utils.py
    + refactored plog()

--- welkin/framework/utils.py
+++ welkin/framework/utils.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import json
+import pprint
 
 logger = logging.getLogger(__name__)
 
@@ -52,18 +53,50 @@ def plog(content):
     """
         Format json content for pretty printing to the logger.
 
-        :param content: json content
-        :return:
+        If `content` is not json, try to identify what it is and then try
+        to format it appropriately.
+
+        :param content: assumed to be json
+        :return formatted_content:
 
         The typical usage will look like this:
         >>> from welkin.framework import utils
         >>> my_json = res.json()
         >>> logger.info(utils.plog.my_json)
     """
-    formatted_content = None
+    # set a default pass-through value of an empty string in order
+    # to catch None, because if a calling method tries to write()
+    # the output of plog() will error out in the attempt.
+    formatted_content = ''
     try:
         formatted_content = json.dumps(content, indent=4, sort_keys=True)
     except (ValueError, TypeError):
         # oops, this wasn't actually json
+
+        # try pretty-printing based on the guessed content type
+        from requests.structures import CaseInsensitiveDict
+        import deepdiff
+
+        if isinstance(content, dict):
+            formatted_content = pprint.pformat(content, indent=1, width=100)
+        elif isinstance(content, list):
+            formatted_content = pprint.pformat(content, indent=1, width=100)
+        elif isinstance(content, CaseInsensitiveDict):
+            formatted_content = pprint.pformat(dict(content), indent=1, width=100)
+        elif isinstance(content, deepdiff.diff.DeepDiff):
+            formatted_content = pprint.pformat(content, indent=1, width=160, depth=4)
+        elif isinstance(content, bytes):
+            # is this XML?
+            if content[:5] == b'<?xml':
+                import xml.dom.minidom
+                xml = xml.dom.minidom.parseString(content)
+                formatted_content = str(xml.toprettyxml())
+            else:
+                # no, this is some other kind of byte string
+                msg = f"Unable to pretty print the content that starts with {content[:20]}"
+                logger.warn(msg)
+                pass
+
         formatted_content = content
+
     return formatted_content
