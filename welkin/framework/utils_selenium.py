e8a0511b867a20ecb4e47d71455bb51bc016d3b2
dsisson <philosophe@gmail.com>
2021-08-19 16:04:48 -0700

    issue 16: support webstorage
    requirements.text
    + added dpath
    
    apps/root_pageobject.py
    + added save_webstorage()
    + updated load_pageobject() to call save_webstorage()
    + added set_event()
    
    framework/utils_webstorage.py
    + added convert_web_storage_data_to_dict()
    
    framework/utils_selenium.py
    + added get_webstorage(), get_local_storage(), clear_local_storage(), get_session_storage(), clear_session_storage()
    
    tests/conftest.py
    + updated set_up_testcase_reporting() to create pytest.welkin_namespace['testrun_webstorage_log_folder'] for ALL apps
    
    framework/utils_file.py
    + added write_webstorage_to_files(), _write_local_to_file(), _write_session_to_file()

--- welkin/framework/utils_selenium.py
+++ welkin/framework/utils_selenium.py
@@ -3,6 +3,10 @@ import time
 import json
 import pytest
 
+from selenium.common.exceptions import WebDriverException
+
+from welkin.framework import utils_webstorage
+
 logger = logging.getLogger(__name__)
 
 
@@ -68,33 +72,33 @@ def extract_text_with_javascript(driver, element):
     return text
 
 
-def get_console_logs(pageobject_instance):
+def get_console_logs(pageobject):
     """
         Get the console for the browser, which may be empty, and write it to
         a file in output/<<testrun>>/<<testcase>>/scanlogs
 
-        :param pageobject_instance:
+        :param pageobject: page object instance
         :return console_logs: list of dicts, see above example
     """
     console_logs = {}
 
-    logger.info(f"\nGetting browser logs for page {pageobject_instance.url}.")
-    console_logs['console'] = pageobject_instance.driver.get_log('browser')
+    logger.info(f"\nGetting browser logs for page {pageobject.url}.")
+    console_logs['console'] = pageobject.driver.get_log('browser')
 
     return console_logs
 
 
-def get_network_traffic_logs(pageobject_instance):
+def get_network_traffic_logs(pageobject):
     """
         Get the performance logs (aka network traffic logs) for the current
         page from the browser.
 
-        :param pageobject_instance:
+        :param pageobject: page object instance
         :return jlogs: list of dicts
     """
-    driver = pageobject_instance.driver
-    url = pageobject_instance.url
-    fname = pageobject_instance.name
+    driver = pageobject.driver
+    url = pageobject.url
+    fname = pageobject.name
 
     logger.info(f"\nGetting network logs for page '{fname}' at {url}.")
     raw_perflogs = driver.get_log('performance')
@@ -107,3 +111,95 @@ def get_network_traffic_logs(pageobject_instance):
              for x in raw_perflogs]
 
     return jlogs
+
+
+def get_webstorage(pageobject):
+    """
+        Wrapper to call methods to get browser local and session storage
+        for the current page and convert to python dicts.
+
+        :param pageobject: page object instance
+        :return: tuple of local storage dict and session storage dict
+    """
+    local = get_local_storage(pageobject)
+    session = get_session_storage(pageobject)
+    return local, session
+
+
+def get_local_storage(pageobject):
+    """
+        Get the window localStorage content for this browser session.
+
+        This uses javascript called by selenium, and then has to do a bunch
+        of cleanup in the returned content.
+
+        :param pageobject: page object instance
+        :return content: dict
+    """
+    # set the javascript logic
+    script = "return { ...window.localStorage };"  # uses JS spread operator
+
+    # run the script
+    content = pageobject.driver.execute_script(script)
+
+    # convert this dirty data into a real dict
+    cleaned_content = utils_webstorage.convert_web_storage_data_to_dict(content)
+    return cleaned_content
+
+
+def clear_local_storage(pageobject):
+    """
+        Delete the local storage for this browser session.
+
+        :param pageobject: page object instance
+        :return: None
+    """
+    # set the javascript logic
+    script = 'window.localStorage.clear();'
+
+    # run the script
+    pageobject.driver.execute_script(script)
+    logger.warning(f"localStorage has been cleared")
+
+
+def get_session_storage(pageobject):
+    """
+        Get the window sessionStorage content for this browser session.
+
+        This uses javascript called by selenium, and then has to do a bunch
+        of cleanup in the returned content.
+
+        :param pageobject: page object instance
+        :return content: dict
+    """
+    # set the javascript logic
+    script = "return { ...window.sessionStorage };"  # uses JS spread operator
+    content = None
+
+    try:
+        # run the script
+        content = pageobject.driver.execute_script(script)
+        # convert this dirty data into a real dict
+        cleaned_content = utils_webstorage.convert_web_storage_data_to_dict(content)
+        return cleaned_content
+    except WebDriverException:
+        # checking for storage immediately after the driver is
+        # loaded will throw a WebDriverException, so ignore that
+        msg = f"Ignoring a WebDriverException."
+        logger.warning(msg)
+        return content
+
+
+def clear_session_storage(pageobject):
+    """
+        Delete the session storage for this browser session.
+
+        :param pageobject: page object instance
+        :return: None
+    """
+    # set the javascript logic
+    script = 'window.sessionStorage.clear();'
+
+    # run the script
+    pageobject.driver.execute_script(script)
+    logger.warning(f"sessionStorage has been cleared")
---
+++ welkin/framework/utils_webstorage.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+import deepdiff
+import dpath.util
+import json
+import logging
+
+from welkin.framework import utils
+
+logger = logging.getLogger(__name__)
+
+
+def convert_web_storage_data_to_dict(content, verbose=False):
+    """
+        Window sessionStorage consists of string keys and string values.
+
+        Convert this into an easier-to-parse data structure that we
+        can traverse.
+
+        Keys are single quoted, and are kept as-is. Values, however,
+        need to be cleaned up. Loop over values and loads as json; if the
+        result is a dict, json-load *that* too.
+
+        :param content: dict, returned from window.sessionContent
+        :param verbose: bool, whether to output additional logging
+        :return new_content: dict, cleaned up data structure
+    """
+    # container for "cleaned" keys and values
+    new_content = {}
+
+    # use a deepcopy to avoid side effects
+    old_content = deepcopy(content)
+
+    def _to_json(data):
+        try:
+            jdata = json.loads(data)
+            return jdata
+        except TypeError:
+            # if it's not a string, that's ok; return the original value
+            return data
+        except json.decoder.JSONDecodeError:
+            # if it's a string but not a dict, that's ok; return original value
+            return data
+
+    # iterate over the top-level keys
+    for k1, v1 in old_content.items():
+        if verbose:
+            logger.info(f"--->> {k1}, {v1}")
+        first_data = _to_json(v1)
+        new_content[k1] = first_data
+        if isinstance(first_data, dict):
+            # iterate over the 2nd-tier keys
+            for k2, v2 in first_data.items():
+                if verbose:
+                    logger.info(f"--->> {k2}, {v2}")
+                second_data = _to_json(v2)
+                new_content[k1][k2] = second_data
+
+    logger.info(f"sessionStorage data converted to dict.")
+    return new_content
