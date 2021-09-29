b641ef80bc368ed8c37eeeb0890ce3dc40435096
dsisson <philosophe@gmail.com>
2021-09-29 10:51:54 -0700

    issue 44: support secure strings from AWS PS
    apps/README_pageobject_model.md
    + added section for "Adding Framework Support for New Applications"
    
    integrations/aws/README_aws.md
    + minor fixes
    
    data/users.py
    + replaced "duckduckgo" users with "dummy_app"
    
    framework/utils.py
    + added strip_password_for_reporting()
    
    framework/utils_file.py
    + added write_sdk_response_to_file()
    
    integrations/aws/aws.py
    + renamed get_password() to get_parameter_data()
    + added support for logging the sdk response, and stripping out the decrypted password value, in get_parameter_data()
    
    models/user.py
    + added decrypt arg to get_password_from_aws(), and added a call to utils.strip_password_for_reporting()
    
    tests/conftest.py
    + added aws() as not a fixture, to manage the creation of the aws session when auth() is used as a test case fixture
    + refactored auth() to call aws()
    + added support for the creation of an 'integrations' folder to set_up_testcase_reporting(), and moved 'auth' into the 'integrations' category of apps; however, always create the integrations folder
    
    tests/integrations/test_aws_integration.py
    + added test_integration_session_fixture_secure_user()
    + updated calls on client.get_password() to client.get_parameter_data()
    + updated every test case to use the appname "dummy_app" and to use utils.strip_password_for_reporting() for any logging of the aws response

--- welkin/integrations/aws/aws.py
+++ welkin/integrations/aws/aws.py
@@ -2,6 +2,7 @@ import logging
 import boto3
 
 from welkin.framework import utils
+from welkin.framework import utils_file
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +24,8 @@ class AWSSession:
         self.session = boto3.Session(region_name=region)
         self.region = region
         if verbose:
-            logger.info(f"\nAWS session created for '{self.region}': {self.session} ({id(self.session)})")
+            logger.info(f"\nAWS session created for '{self.region}': "
+                        f"{self.session} ({id(self.session)})")
 
 
 class AWSClient:
@@ -40,6 +42,7 @@ class AWSClient:
             :param session: boto3 session object
             :param resource_name: str, service/resource identifier
         """
+        self.name = 'AWS client'
         # extract the boto3 object from the Welkin object
         self.session = session.session
         self.region = session.region
@@ -48,16 +51,18 @@ class AWSClient:
         client = self.session.client(resource_name)
         self.client = client
 
-    def get_password(self, aws_key_name, decrypt=False):
+    def get_parameter_data(self, aws_key_name, decrypt=False):
         """
-            Get the value for key `aws_key_name` from the AWS
-            Parameter store.
+            Get the value (and all the associated meta data) for key
+            `aws_key_name` from the AWS Parameter store.
 
-            This is really for any parameter in the AWS Parameter Store,
-            but for disambiguation we call this a password operation.
+            If this used for a password, set the parameter value in AWS
+            as a Secure String. This will encrypt the value using the
+            AWS KMS service, so if you are getting a encrypted password,
+            pass the `decrypt=True` argument.
 
             See the README for more context as well as AWS set up
-            information to support passwords (parameters).
+            information to support passwords as parameters.
 
             :param aws_key_name: str, key name (looks likes a path)
             :param decrypt: bool, True or False; defaults to False
@@ -65,4 +70,9 @@ class AWSClient:
         """
         res = self.client.get_parameter(Name=aws_key_name,
                                         WithDecryption=decrypt)
+        # write the response headers to a file
+        utils_file.write_sdk_response_to_file(
+            utils.strip_password_for_reporting(res),
+            sdk_app=self.name,
+            fname='get param')
         return res
--- welkin/tests/integrations/test_aws_integration.py
+++ welkin/tests/integrations/test_aws_integration.py
@@ -17,10 +17,10 @@ class AwsIntegrationTests(object):
         """
         aws_region = 'us-west-1'
         tier = TIER
-        appname = 'duckduckgo'
+        appname = 'dummy_app'
         user = 'user01'
         param_key = f"/welkin/{tier}/{appname}/{user}"
-        expected_param_value = f"password for user \"{param_key}\""
+        expected_param_value = f"secure password for user \"{param_key}\""
 
         # create a session object tied to the local default config
         # for region and IAM user
@@ -30,8 +30,9 @@ class AwsIntegrationTests(object):
         client = AWSClient(session, resource_name='ssm')
 
         # make the AWS get parameter call
-        res = client.get_password(aws_key_name=param_key, decrypt=False)
-        logger.info(f"\nAWS response:\n{utils.plog(res)}")
+        res = client.get_parameter_data(aws_key_name=param_key, decrypt=True)
+        logger.info(f"\nAWS response:\n"
+                    f"{utils.plog(utils.strip_password_for_reporting(res))}")
 
         # check the expected parameter (aka the "password")
         actual_param_value = res['Parameter']['Value']
@@ -42,10 +43,10 @@ class AwsIntegrationTests(object):
             Validation for the AWS integration code with default args.
         """
         tier = TIER
-        appname = 'duckduckgo'
+        appname = 'dummy_app'
         user = 'user01'
         param_key = f"/welkin/{tier}/{appname}/{user}"
-        expected_param_value = f"password for user \"{param_key}\""
+        expected_param_value = f"secure password for user \"{param_key}\""
 
         # create a session object tied to the local default config
         # for region and IAM user
@@ -55,8 +56,9 @@ class AwsIntegrationTests(object):
         client = AWSClient(session, resource_name='ssm')
 
         # make the AWS get parameter call
-        res = client.get_password(aws_key_name=param_key, decrypt=False)
-        logger.info(f"\nAWS response:\n{utils.plog(res)}")
+        res = client.get_parameter_data(aws_key_name=param_key, decrypt=True)
+        logger.info(f"\nAWS response:\n"
+                    f"{utils.plog(utils.strip_password_for_reporting(res))}")
 
         # check the expected parameter (aka the "password")
         actual_param_value = res['Parameter']['Value']
@@ -72,18 +74,19 @@ class AwsIntegrationTests(object):
         """
         # setup
         tier = TIER
-        appname = 'duckduckgo'
+        appname = 'dummy_app'
         user = 'user01'
         param_key = f"/welkin/{tier}/{appname}/{user}"
-        expected_param_value = f"password for user \"{param_key}\""
+        expected_param_value = f"secure password for user \"{param_key}\""
         session = auth
 
         # create a client tied to this session
         client = AWSClient(session, resource_name='ssm')
 
         # make the AWS get parameter call
-        res = client.get_password(aws_key_name=param_key, decrypt=False)
-        logger.info(f"\nAWS response:\n{utils.plog(res)}")
+        res = client.get_parameter_data(aws_key_name=param_key, decrypt=True)
+        logger.info(f"\nAWS response:\n"
+                    f"{utils.plog(utils.strip_password_for_reporting(res))}")
 
         # check the expected parameter (aka the "password")
         actual_param_value = res['Parameter']['Value']
@@ -99,18 +102,19 @@ class AwsIntegrationTests(object):
         """
         # setup
         tier = TIER
-        appname = 'duckduckgo'
+        appname = 'dummy_app'
         user = 'user01'
         param_key = f"/welkin/{tier}/{appname}/{user}"
-        expected_param_value = f"password for user \"{param_key}\""
+        expected_param_value = f"secure password for user \"{param_key}\""
         session = auth
 
         # create a client tied to this session
         client = AWSClient(session, resource_name='ssm')
 
         # make the AWS get parameter call
-        res = client.get_password(aws_key_name=param_key, decrypt=False)
-        logger.info(f"\nAWS response:\n{utils.plog(res)}")
+        res = client.get_parameter_data(aws_key_name=param_key, decrypt=True)
+        logger.info(f"\nAWS response:\n"
+                    f"{utils.plog(utils.strip_password_for_reporting(res))}")
 
         # check the expected parameter (aka the "password")
         actual_param_value = res['Parameter']['Value']
@@ -123,10 +127,10 @@ class AwsIntegrationTests(object):
         """
         # setup
         tier = TIER
-        appname = 'duckduckgo'
+        appname = 'dummy_app'
         user_id = 'user01'
         param_key = f"/welkin/{tier}/{appname}/{user_id}"
-        expected_param_value = f"password for user \"{param_key}\""
+        expected_param_value = f"secure password for user \"{param_key}\""
         session = auth
 
         # set up user object
@@ -134,7 +138,36 @@ class AwsIntegrationTests(object):
         logger.info(f"\n~~~~> user object:\n{user}")
 
         # get the password
-        user.get_password_from_aws(session, verbose=True)
+        user.get_password_from_aws(session, decrypt=True, verbose=True)
+        logger.info(f"\n~~~~> user object:\n{user}")
+
+        # check the expected parameter (aka the "password")
+        # NOTE: CAREFUL WHEN ACCESSING OR COMPARING PASSWORDS
+        actual_param_value = user.password
+        assert actual_param_value == expected_param_value
+
+    def test_integration_session_fixture_secure_user(self, auth):
+        """
+            Validation for the AWS integration code called as a fixture,
+            and using the User model.
+        """
+        # setup
+        tier = TIER
+        appname = 'dummy_app'
+        user_id = 'user01'
+        # this param_key is just used here for logging and;
+        # the key used in the AWS called is generated in the
+        # user model
+        param_key = f"/welkin/{tier}/{appname}/{user_id}"
+        expected_param_value = f"secure password for user \"{param_key}\""
+        session = auth
+
+        # set up user object
+        user = ApplicationUser(tier, appname, user_id)
+        logger.info(f"\n~~~~> user object:\n{user}")
+
+        # get the password
+        user.get_password_from_aws(session, verbose=True, decrypt=True)
         logger.info(f"\n~~~~> user object:\n{user}")
 
         # check the expected parameter (aka the "password")
